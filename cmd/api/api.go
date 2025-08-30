6fcb7f39001f75f6608c6169d35f8450d1ab2971
TheLuminousArtemis <111784883+TheLuminousArtemis@users.noreply.github.com>
2025-08-30 08:05:04 +0000

    graceful shutdown

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -41,6 +47,31 @@ func (app *application) mount() *chi.Mux {
 }
 
 func (app *application) start(r http.Handler) error {
+	srv := http.Server{
+		Addr:    fmt.Sprintf(":%d", app.config.Port),
+		Handler: r,
+	}
+	shutdown := make(chan error)
+	go func() {
+		quit := make(chan os.Signal, 1)
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		s := <-quit
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		app.logger.Info("signal caught", "signal", s.String())
+		shutdown <- srv.Shutdown(ctx)
+	}()
+
 	app.logger.Info("Starting proxy server", "port", app.config.Port, "origin", app.config.Origin)
-	return http.ListenAndServe(fmt.Sprintf(":%d", app.config.Port), r)
+	err := srv.ListenAndServe()
+	if err != nil && !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+	err = <-shutdown
+	if err != nil {
+		return err
+	}
+	app.logger.Info("server has stopped", "addr", app.config.Port)
+	return nil
+
 }
